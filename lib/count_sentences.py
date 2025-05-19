c86749f443796d07993c0276ca53267b0846be4c
Mercy <owinomercy983@gmail.com>
2025-05-19 15:32:23 +0300

    counting sentenses

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+    def __init__(self, sentence=""):
+        self._sentence = ""
+        self.sentence = sentence
+
+    @property
+    def sentence(self):
+        return self._sentence
+
+    @sentence.setter
+    def sentence(self, value):
+        if isinstance(value, str):
+            self._sentence = value
+        else:
+            print("The value must be a string.")
+
+    # Alias `value` to `sentence`
+    value = property(
+        fget=lambda self: self.sentence,
+        fset=lambda self, val: setattr(self, 'sentence', val)
+    )
+    
+    def is_sentence(self):
+        return self._sentence.endswith(".")
+
+    def is_question(self):
+        return self._sentence.endswith("?")
+
+    def is_exclamation(self):
+        return self._sentence.endswith("!")
+
+    def count_sentences(self):
+        return len(re.findall(r'[^.!?]+[.!?]', self._sentence))
